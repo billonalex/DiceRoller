feeae81182e98614d2f7c3da4e8c2c2dd8b994e9
Alexandre Billon <abi@atolcd.com>
2021-11-16 11:01:08 +0100

    [Add] dice routes

--- api/repository/diceRepository.py
+++ api/repository/diceRepository.py
@@ -1,12 +1,45 @@
 import random
 
+from models.dice import Dice
 from models.user import User
 from models.response import Response
 from db.db import connect
 
 class DiceRepository():
     def get_dices(self):
-        return True
+        conn = connect()
+        cursor = conn.cursor();
+
+        cursor.execute("""
+        SELECT
+            iddice,
+            idutilisateur,
+            valeur
+        FROM dice""")
+
+        res = cursor.fetchall()
+        liste = [Dice(**element) for element in res]
+        dices = {
+            "dices": liste,
+            "nb_row": len(liste),
+        }
+
+        return dices
+
+    def get_dice_by_id(self, iddice):
+        conn = connect()
+        cursor = conn.cursor();
+
+        cursor.execute("""
+        SELECT
+            iddice,
+            idutilisateur,
+            valeur
+        FROM dice
+        WHERE iddice = :iddice""", {"iddice": iddice})
+
+        res = cursor.fetchone()
+        return res
 
     def throw_new_dice(self, idutilisateur):
         conn = connect()
@@ -22,3 +55,5 @@ class DiceRepository():
         })
 
         conn.commit()
+
+        return self.get_dice_by_id(cursor.lastrowid)
--- api/service/diceService.py
+++ api/service/diceService.py
@@ -23,4 +23,22 @@ dice = APIRouter(
     response_model=DiceList
 )
 def get_dices(current_user: User = Depends(authRepository.get_current_active_user)):
-    return True
+    return diceRepository.get_dices()
+
+@dice.get(
+    path="/{iddice}",
+    name="Récupération d'un dé",
+    description="Permet la récupération d'un dé.",
+    response_model=Dice
+)
+def get_dices(iddice: int, current_user: User = Depends(authRepository.get_current_active_user)):
+    return diceRepository.get_dice_by_id(iddice)
+
+@dice.post(
+    path="/",
+    name="Nouveau lancer de dés",
+    description="Permet un nouveau lancer de dés.",
+    response_model=Dice
+)
+def throw_new_dice(current_user: User = Depends(authRepository.get_current_active_user)):
+    return diceRepository.throw_new_dice(current_user.idutilisateur)
